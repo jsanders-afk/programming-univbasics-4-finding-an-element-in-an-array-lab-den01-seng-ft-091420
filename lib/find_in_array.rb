907b89b9e71a6786a1f2a296c08f6406d7a8daf6
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-31 06:17:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,12 +4,20 @@ array = (1..100).to_a
 
 
 def find_element_index(array, value_to_find)
+  if array.include?(value_to_find)  
     i = 0 
-     while array[i] do
+    while array[i] do
        break if array[i] == value_to_find
        i += 1
      end  
      i
+
+
+
+
+
+
+
 end
 binding.pry
   
